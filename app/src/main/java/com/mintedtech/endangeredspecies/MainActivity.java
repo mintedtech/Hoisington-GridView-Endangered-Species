54990c5f23ca96b169c26325fb9befc7a6f792b7
SA <mintedtech@gmail.com>
2023-11-29 23:58:04 -0500

    Minor Code Cleanup (Variables -> Finals, et al.)

--- app/src/main/java/com/mintedtech/endangeredspecies/MainActivity.java
+++ app/src/main/java/com/mintedtech/endangeredspecies/MainActivity.java
@@ -17,55 +17,57 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     // array of images - each image is automatically assigned an ID, so we use type int below
-    int[] animals = {R.drawable.elephant, R.drawable.gorilla, R.drawable.leopard,
+    private final int[] ANIMAL_DRAWABLES =
+            {R.drawable.elephant, R.drawable.gorilla, R.drawable.leopard,
             R.drawable.panda, R.drawable.polar, R.drawable.rhino};
 
-    String[] animalNames = {"Elephant", "Gorilla", "Leopard",
+    // parallel array - name of each image corresponding to image in array above
+    private final String[] ANIMAL_NAMES = {"Elephant", "Gorilla", "Leopard",
             "Panda", "Polar Bear", "Rhino"};
 
-    ImageView pic; // imgLarge (bottom half of the screen)
+    private ImageView largeImage; // imgLarge (bottom half of the screen)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        largeImage = findViewById(R.id.imgLarge);
+        setupGridView();
+    }
 
+    private void setupGridView() {
         GridView grid = findViewById(R.id.gridView);
-        pic = findViewById(R.id.imgLarge);
-
         grid.setAdapter(new ImageAdapter());
         grid.setOnItemClickListener((parent, view, position, id) -> handleItemClick(view, position));
-
     }
 
     private void handleItemClick(View view, int position) {
         // Tell the user which image number they clicked on in the GridView
         Snackbar.make(view,
-                        "Selected Species: " + (position + 1) + " - " + animalNames[position],
+                        "Selected Species: " + (position + 1) + " - " + ANIMAL_NAMES[position],
                         Snackbar.LENGTH_LONG)
                 .show();
         // Set the bottom ImageView's image to the image they clicked on in the GridView
-        pic.setImageResource(animals[position]);
+        largeImage.setImageResource(ANIMAL_DRAWABLES[position]);
     }
 
     private class ImageAdapter extends BaseAdapter {
-        float cornerRadius;
-        ShapeAppearanceModel roundedCornersShapeAppearanceModel;
+        private final ShapeAppearanceModel ROUNDED_CORNERS_SHAPE_APPEARANCE_MODEL;
 
         public ImageAdapter() {
-            cornerRadius = getResources().getDimension(R.dimen.standard_margin);
-            roundedCornersShapeAppearanceModel =
+            float cornerRadius = getResources().getDimension(R.dimen.standard_margin);
+            ROUNDED_CORNERS_SHAPE_APPEARANCE_MODEL =
                     ShapeAppearanceModel.builder().build().withCornerSize(cornerRadius);
         }
 
         @Override
         public int getCount() {
-            return animals.length; // this equals six, per her design -- number of animals in Grid
+            return ANIMAL_DRAWABLES.length; // this equals six, per her design -- number of animals in Grid
         }
 
         @Override
         public Object getItem(int position) {
-            return animalNames[position];
+            return ANIMAL_NAMES[position];
         }
 
         @Override
@@ -77,16 +79,16 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             // If this method was called to fill an empty portion of the grid then create new...
             // but if this is updating due to scrolling, then use the existing ImageView in the grid
-            ShapeableImageView pic = convertView == null ?
+            ShapeableImageView gridImage = convertView == null ?
                     new ShapeableImageView(MainActivity.this) :
                     (ShapeableImageView) convertView;
 
             // Update the image in this ImageView to the currently clicked animal
-            pic.setImageResource(animals[position]);
-            pic.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            pic.setShapeAppearanceModel(roundedCornersShapeAppearanceModel);
+            gridImage.setImageResource(ANIMAL_DRAWABLES[position]);
+            gridImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            gridImage.setShapeAppearanceModel(ROUNDED_CORNERS_SHAPE_APPEARANCE_MODEL);
 
-            return pic;
+            return gridImage;
         }
     }
 }
